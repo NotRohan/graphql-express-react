1ad0a58e2ba295e286e0be4457ed61f281ce3ded
Rohan <rohan.sharma@cleartrip.com>
2019-04-10 16:38:07 +0530

    added mongodb and read update operations

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
+const mongoose = require('mongoose')
+const Event = require('./models/events')
+
 const app = express()
+
 const {
     buildSchema
 } = require('graphql')
@@ -10,11 +14,24 @@ app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         schema {
             query: RootQuery
@@ -23,14 +40,41 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return ['Romantic', 'Cooking', 'Sailing']
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return {
+                            ...event._doc
+                        };
+                    })
+                }).catch(err => {
+                    throw err;
+                })
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            })
+            return event.save().then(result => {
+                console.log(result)
+                return {
+                    ...result._doc
+                };
+            }).catch(err => {
+                console.log(err)
+                throw err
+            });
         }
     },
     graphiql: true
 }))
 
-app.listen(3000)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-pmmwt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
+    .then(() => {
+        app.listen(3000)
+    }).catch(err => {
+        console.log(err);
+    })
