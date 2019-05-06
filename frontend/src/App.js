5b3d5e8a750b21a4d61d3812e886393900f5c5a4
Rohan <rohan.sharma@cleartrip.com>
2019-05-06 12:37:02 +0530

    frotend making

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import './App.css';
+import AuthPage from './pages/Auth'
+import BookingsPage from './pages/Bookings'
+import EventsPage from './pages/Events'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>It works</h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
